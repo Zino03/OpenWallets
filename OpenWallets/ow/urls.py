943682e4b9c444e60df4218736f28eb08ede3cf0
Zino03 <zldnl454@naver.com>
2025-05-22 15:39:58 +0900

    의원 리스트 정렬 기능 완료, 의원 상세 페이지 정보 완료

--- OpenWallets/ow/urls.py
+++ OpenWallets/ow/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('members/', views.member_list, name="member_list") ,
-    path('members/<str:member_code>/', views.member_info, name='member_info'),
+    path('members/<str:member_id>/', views.member_info, name='member_info'),
     path('api/', views.api_page, name="api_page") ,
     path('guide/', views.guide_page, name="guide_page") ,
 ]
